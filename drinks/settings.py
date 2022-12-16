357686c75807fd1aed6412352bf356f14b1c9f2b
anoopraju31 <59496739+anoopraju31@users.noreply.github.com>
2022-12-16 19:13:39 +0530

    Update settings.py

--- drinks/settings.py
+++ drinks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=nj0@j@n+yjb8(-2))vn7ef%93w3y!t)45@^7c3+_qwk(xso0@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
